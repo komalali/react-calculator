50f98721b316f861133058ca6a00465f9da34218
Komal Ali <komalali@uw.edu>
2018-02-25 22:58:04 -0800

    moved calculator to its own file

--- src/App.js
+++ src/App.js
@@ -1,108 +1,15 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
+import React, {Component} from 'react';
+import Calculator from './components/Calculator';
+
 import './App.css';
 import '../node_modules/semantic-ui-css/semantic.min.css';
 
-const ActionKey = (props) => {
-  return (
-    <div className="key column">
-      <button className="fluid ui orange button">{props.value}</button>
-    </div>
-  )
-};
-
-const NumberKey = (props) => {
-  return (
-    <div className="key column">
-      <button className="fluid ui blue button">{props.value}</button>
-    </div>
-  )
-};
-
-const KeyPad = () => {
-  return (
-    <div className="keypad row">
-      <div className="numberPad three wide column">
-        <div className="ui three column grid container">
-          <div className="numberPad-row row">
-            <NumberKey value="1" />
-            <NumberKey value="2" />
-            <NumberKey value="3" />
-          </div>
-          <div className="numberPad-row row">
-            <NumberKey value="4" />
-            <NumberKey value="5" />
-            <NumberKey value="6" />
-          </div>
-          <div className="numberPad-row row">
-            <NumberKey value="7" />
-            <NumberKey value="8" />
-            <NumberKey value="9" />
-          </div>
-          <div className="numberPad-row row">
-            <NumberKey value="0" />
-            <NumberKey value="00" />
-            <NumberKey value="." />
-          </div>
-        </div>
-      </div>
-      <div className="actionPad three wide column">
-        <div className="ui two column grid container">
-          <ActionKey value="+"/>
-          <ActionKey value="-"/>
-        </div>
-        <div className="ui two column grid container">
-          <ActionKey value="/"/>
-          <ActionKey value="*"/>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const ScreenRow = (props) => {
-  return (
-    <div className={`${props.type} six wide column`}>
-      <div className="" style={{textAlign: "right"}}>
-        {props[props.type]}
-      </div>
-    </div>
-  )
-};
-
-const Screen = (props) => {
-  return (
-    <div className="screen row ui segment">
-      <ScreenRow type="total" total={props.total} />
-      <ScreenRow type="input" input={props.input} />
-    </div>
-  );
-};
-
-class Calculator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: ' ',
-      total: 0,
-    };
-  }
-
-  render() {
-    return (
-      <div className="calculator ui raised very padded container segment">
-        <Screen input={this.state.input} total={this.state.total}/>
-        <KeyPad/>
-      </div>
-    );
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Calculator/>
       </div>
     );
   }
---
+++ src/components/Calculator.js
@@ -0,0 +1,218 @@
+// Calculator.js
+import React, {Component} from 'react';
+import _ from 'lodash';
+import {
+  Button,
+  Container,
+  Grid,
+  Segment
+} from 'semantic-ui-react';
+
+const Key = (props) => {
+  const keyColor = () => {
+    switch (props.color) {
+      case undefined:
+        return "blue";
+      default:
+        return props.color;
+    }
+  };
+
+  return (
+    <Grid.Column className="key" style={{padding: "0 5px"}}>
+      <Button color={keyColor()}
+              compact
+              fluid
+              onClick={() => props.keyClick(props.value)}
+              size="huge"
+              style={{fontFamily: "Open Sans", fontSize:"20px", fontWeight: "regular", margin: "0", padding: "10px 0"}}>
+        {props.value}
+      </Button>
+    </Grid.Column>
+  )
+};
+
+const KeyPad = (props) => {
+  const rowStyle = {
+    padding: '5px 0'
+  };
+
+  return (
+    <Grid columns={5} style={{marginTop: "10px"}}>
+      <Grid.Row style={rowStyle}>
+        <Key value="1" keyClick={props.keyClick}/>
+        <Key value="2" keyClick={props.keyClick}/>
+        <Key value="3" keyClick={props.keyClick}/>
+        <Key value="+" keyClick={props.keyClick} color="teal"/>
+        <Key value="/" keyClick={props.keyClick} color="teal"/>
+      </Grid.Row>
+      <Grid.Row style={rowStyle}>
+        <Key value="4" keyClick={props.keyClick}/>
+        <Key value="5" keyClick={props.keyClick}/>
+        <Key value="6" keyClick={props.keyClick}/>
+        <Key value="-" keyClick={props.keyClick} color="teal"/>
+        <Key value="*" keyClick={props.keyClick} color="teal"/>
+      </Grid.Row>
+      <Grid.Row style={rowStyle}>
+        <Key value="7" keyClick={props.keyClick}/>
+        <Key value="8" keyClick={props.keyClick}/>
+        <Key value="9" keyClick={props.keyClick}/>
+      </Grid.Row>
+      <Grid.Row style={rowStyle}>
+        <Key value="0" keyClick={props.keyClick}/>
+        <Key value="00" keyClick={props.keyClick}/>
+        <Key value="." keyClick={props.keyClick}/>
+        <Key value="clear" keyClick={props.keyClick} color="purple"/>
+        <Key value="=" keyClick={props.keyClick} color="purple"/>
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+const ScreenRow = (props) => {
+  return (
+    <Segment className={props.type} vertical style={props.style}>
+      {props[props.type]}
+    </Segment>
+  )
+};
+
+const Screen = (props) => {
+  return (
+    <Segment.Group className="screen" style={{background: "white"}}>
+      <ScreenRow type="total" total={props.total} style={{fontFamily: 'Open Sans', fontSize: "30px", padding: "10px 0"}}/>
+      <ScreenRow type="input" input={props.inputString} style={{fontFamily: 'Open Sans', fontSize: "20px", padding: "10px 0"}}/>
+    </Segment.Group>
+  );
+};
+
+export default class Calculator extends Component {
+  static initialState = () => ({
+    inputNumbers: [],
+    inputActions: [],
+    inputString: '',
+    total: '0',
+    internalTotal: 0,
+  });
+
+  actionKeys = ['+', '-', '/', '*'];
+
+  state = Calculator.initialState();
+
+  add = (priorTotal, number) => priorTotal + number;
+
+  subtract = (priorTotal, number) => priorTotal - number;
+
+  multiply = (priorTotal, number) => priorTotal * number;
+
+  divide = (priorTotal, number) => priorTotal / number;
+
+  keyClick = (clickedKey) => {
+    switch (clickedKey) {
+      case "=":
+        this.processInput();
+        break;
+      case "clear":
+        this.clearInput();
+        break;
+      default:
+        this.updateInput(clickedKey);
+        break;
+    }
+  };
+
+  clearInput = () => {
+    console.log('clear');
+    this.setState(Calculator.initialState)
+  };
+
+  processInput = (clickedKey) => {
+    console.log('process');
+    this.setState(prevState => {
+      const { inputActions, inputNumbers, internalTotal } = prevState;
+      if (internalTotal === 0) {
+        console.log('howdy');
+        switch (inputActions[0]) {
+          case "+":
+            return {
+              internalTotal: this.add(inputNumbers[0], inputNumbers[1])
+            };
+          case "-":
+            return {
+              internalTotal: this.subtract(inputNumbers[0], inputNumbers[1])
+            };
+          case "/":
+            return {
+              internalTotal: this.divide(inputNumbers[0], inputNumbers[1])
+            };
+          case "*":
+            return {
+              internalTotal: this.multiply(inputNumbers[0], inputNumbers[1]),
+              total: `${internalTotal}`
+            };
+          default:
+            break;
+        }
+      }
+    });
+  };
+
+  updateInput = (clickedKey) => {
+    console.log('update');
+
+    if (this.actionKeys.includes(clickedKey) && this.state.inputActions.length > 0) {
+      this.processInput()
+    }
+
+    this.setState(prevState => {
+      const { inputActions, inputString, total } = prevState;
+      if (this.actionKeys.includes(clickedKey)) {
+        if (total === '0' && this.actionKeys.includes(inputString[inputString.length -1])) {
+          return {
+            inputActions: inputActions.slice(0, inputActions.length - 1).concat(clickedKey),
+            inputString: `${inputString.substring(0, inputString.length - 1)}${clickedKey}`,
+          }
+        } else {
+          return {
+            inputNumbers: prevState.inputNumbers.concat(Number(total)),
+            inputActions: prevState.inputActions.concat(clickedKey),
+            inputString: `${prevState.inputString} ${total} ${clickedKey}`,
+            total: '0',
+          }
+        }
+      } else {
+        if (total === '0') {
+          switch (clickedKey) {
+            case '0':
+            case '00':
+              break;
+            case '.':
+              return {total: `${prevState.total}${clickedKey}`};
+            default:
+              return {total: clickedKey}
+          }
+        } else if (total.includes('.')) {
+          switch (clickedKey) {
+            case '.':
+              break;
+            default:
+              return {total: `${prevState.total}${clickedKey}`};
+          }
+        } else {
+          return {total: `${prevState.total}${clickedKey}`}
+        }
+      }
+    });
+  };
+
+  render() {
+    return (
+      <Container className="calculator">
+        <Segment raised={true} padded="very" style={{background: "whiteSmoke"}}>
+          <Screen inputString={this.state.inputString} total={this.state.total}/>
+          <KeyPad keyClick={this.keyClick}/>
+        </Segment>
+      </Container>
+    )
+  }
+}
